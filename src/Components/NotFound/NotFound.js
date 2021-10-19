9bdce308dafeb5a3384f01c0a02cfbe421303573
Farshid-997 <arnob.farshid@gmail.com>
2021-10-19 23:23:05 +0600

    updated

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import img1 from './dentalcare.jpg'
 const NotFound = () => {
     return (
         <div>
-            <h1>Page not found</h1>
+            <img src={img1} alt=""></img>
+
         </div>
     );
 };
